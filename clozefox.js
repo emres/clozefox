4578421df85b098f8a43774fceaebbac828879ce
Emre Sevinc <Frederik Cornillie@.(none)>
2010-01-22 09:38:02 +0100

    Minor code clean-up.

--- clozefox.js
+++ clozefox.js
@@ -149,9 +149,6 @@ function runClozeFox() {
    
     var doc = jetpack.tabs.focused.contentDocument; 
 
-    // $(doc).find("div div:only-child").css("border", "3px blue solid");
-    // $(doc).find("div div:only-child").each(function (index) {
-
     $(doc).find("div > p").each(function (index) {	
 	var textStr = $(this).text();
 	/*
@@ -165,11 +162,6 @@ function runClozeFox() {
 	}
     });
     
-
-    // for(var i = 0; i < results.length; i++) {
-    // 	$(doc).find("#clozefox_paragraph_" + results[i]).remove();
-    // }         
-
     /*
       If the enough amount of text cannot be found or grabbed correctly 
       by the system then immediately stop processing
@@ -583,17 +575,6 @@ function suggestPage(pageType) {
 
     disableCalculateScore();
     enableTests();
-
-    // $.ajax({
-    // 	type: "GET",
-    // 	url: "http://feeds.delicious.com/v2/rss/YAFZ/clozefox",
-    // 	datatype: "xml",
-    // 	success: function(xml) {
-    // 	    // var deliciousResponse = xml;
-    // 	    jetpack.notifications.show(xml);
-    // 	}
-    // });
-
 }
 
 function testJQ() {
@@ -620,25 +601,6 @@ function testJQ() {
 		style.innerHTML = js;
 		doc.getElementsByTagName('HEAD')[0].appendChild(style);
 				
-		// var script = doc.createElement("script");
-		// script.innerHTML  = 'var myDragFunc = function(){';
-                // script.innerHTML +=    '$("<style type=text/css> .drg_style {margin:5px; padding:10px; width:250px; height:25px; background:green} </style>").appendTo("body");';
-                // script.innerHTML +=    '$("<div> <div class=drg_style>Div1</div> <div class=drg_style>Div2</div> </div>").appendTo("body");';
-		// script.innerHTML +=    '$(".drg_style").draggable();';
-	        // script.innerHTML +=   '};';
-		// doc.body.appendChild(script);		
-		// win.wrappedJSObject['myDragFunc']();
-
-		// script = doc.createElement("script");
-		// script.innerHTML += 'var myDialogFunc = function () {';
-		// script.innerHTML +=  '$("<div id=dialog title=\\"Basic Dialog\\"> <p>The dialog window can be moved, resized and closed with the X icon.</p></div>").appendTo("body");';
-		// script.innerHTML += '$("#dialog").dialog({'
-		// script.innerHTML += '      bgiframe: true, height: 140, modal: true';
-		// script.innerHTML += '  });';
-		// script.innerHTML += '};';
-		// doc.body.appendChild(script);
-		// win.wrappedJSObject['myDialogFunc']();
-
 		script = doc.createElement("script");
 
 		script.innerHTML = 'var myDialogFunc = function () {';
@@ -646,32 +608,7 @@ function testJQ() {
 		script.innerHTML += '$("#dialog").dialog({'
 		script.innerHTML += '      bgiframe: true, height: 140, modal: true';
 		script.innerHTML += '  });';
-		script.innerHTML += '};';
-		
-		// var dialogObject = {
-		//     jsCode : (<><![CDATA[
-		// 	var myDialogFunc = function () {
-		// 	    // $("<div id=dialog title=Basic> <p>The dialog window can be moved, resized and closed with the X icon.</p></div>").appendTo("body");
-		// 	    $("#dialog").dialog({
-		// 		bgiframe: true, 
-		// 		height: 140, 
-		// 		modal: true
-		// 	    });
-		// 	};
-		//    ]]</>).toString();
-		// }
-		// script.innerHTML = dialogObject.jsCode;
-
-		// var test_object = {
-		//     javascript_code: <> function ()  {return true;} </>,
-		//     initialize: function(){
-		// 	var doc = jetpack.tabs.focused.contentDocument;
-		// 	var script = doc.createElement('script');
-		// 	doc.body.appendChild(script);
-		// 	script.innerHTML = test_object.javascript_code;
-		//     }
-		// }
-
+		script.innerHTML += '};';		
 		
 		doc.body.appendChild(script);
 		win.wrappedJSObject['myDialogFunc']();		
@@ -787,6 +724,7 @@ jetpack.tabs.onClose(function () {
 	enableTests();
     }
 });
+
 
 //
 // Dictionary Jetpack
